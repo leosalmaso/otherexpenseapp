a11ee9219ca5a13a6ab33cac0fafdb11511afc65
Leo Salmaso <leonardo.salmaso@gmail.com>
2020-01-22 23:25:57 -0500

    initial commit

---
+++ otherexpenseapp/otherexpenseapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  otherexpenseapp
+//
+//  Created by Leo Salmaso on 1/22/20.
+//  Copyright © 2020 Leo Salmaso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
